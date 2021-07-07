4cf8c455b32b1ffcd717d4294c35ca73cc4470c3
sanammalik04 <sanam.malik04@gmail.com>
2021-07-07 14:09:02 -0400

    completed plusMinus algo

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-console.log('hello world');
+// Data Types in js
 
+// var myName = 'Sanam Malik';
+// console.log(myName);
+
+// console.log(typeof(myName));
+
+// var iAmSanama = false;
+// console.log(iAmSanama)
+
+// console.log(typeof(iAmSanama))
+
+//console.log(10 + "20") // 1020
+
+//console.log(9 - "5")  // 4 -- bug 
+
+// console.log("java" + "script") // javascript
+
+// console.log(" " + " ") // just space
+
+// console.log("" + 0) // 0
+
+// console.log('sanam' - 'malik') //not a number NaN
+
+//console.log(true + true) //2 - in js 1 represents 2
+//console.log(true + false) // 1 -- in js 0 represents false
+
+//console.log(false + true) // 1
+
+// console.log(false - true) // -1 becasue false is always 0 and true is always 1
+
+
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless))
+
+// var iAmStandBy;
+// console.log(iAmStandBy);
+
+// NaN === NaN;
+// Number.NaN === NaN;
+// isNaN(NaN);
+// isNaN(Number.NaN);
+// Number.isNaN(NaN);
+
+// console.log((Number.isNaN(NaN)))
+
+// console.log(5+20);
+
+//6 types of expressions and operators
+//assignment operater- assigns a value to its left operand: var x = 5 
+
+// var x = 5;
+// var y = 5;
+// console.log(`is both the x and y are equal : ${x == y}`);
 
---
+++ plusMinus.js
@@ -0,0 +1,36 @@
+// Given an array of integers, calculate the ratios of its elements that are positive, 
+// negative, and zero. 
+// Print the decimal value of each fraction on a 
+//new line with  places after the decimal.
+
+
+let arr = [-4, 3, -9, 0, 4, 1]
+function plusMinus(arr){
+    let n = arr.length;
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+
+    for(let i = 0; i < n; i++){
+        if(arr[i] > 0){
+            positive += 1;
+        } else if(arr[i] < 0){
+            negative += 1;
+        } else {
+            zero += 1;
+        }
+    }
+    let pos = positive/n;
+    let neg = negative/n;
+    let zer = zero/n;
+
+    console.log(pos.toFixed(6) + "\n" + neg.toFixed(6) + "\n" + zero.toFixed(6))
+
+    
+}
+console.log(plusMinus(arr))
+
+
+
+
+

766d6d124ac74ad50a92dc1d540ca796942b1220
sanammalik04 <sanam.malik04@gmail.com>
2021-07-14 10:27:13 -0400

    started new problem

---
+++ EasyAlgosHack/targetTwoSum.js
@@ -0,0 +1,16 @@
+// Given an array of unsorted unique integers, write a function getTargetIndexes that returns an array of arrays of the 
+// index pairs for each of the two numbers that add up to the target.
+
+// Prioritize run time complexity over memory
+// An item can not match with itself
+// The same pair of indexes should only be included once
+// The order of the returned array and its items does not matter
+
+
+const array = [4, -3, 80, 2, 9, 13, 5, 8];
+const target = 10;
+const getTargetIndexes = (array, target) => {
+    
+};
+
+console.log(getTargetIndexes(array, target))
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# js-projects
+Solving problems from hackerrank and leetcode to solidify my understanding of algorithms and data structures.
